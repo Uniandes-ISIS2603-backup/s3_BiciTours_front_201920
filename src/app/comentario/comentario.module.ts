8d7fe87bcdf19db76a4259912efb19d41c4f0273
Oscar Julian Castañeda G <oj.castaneda@uniandes.com>
2019-11-08 16:50:15 -0500

    Correccion comentario.module.ts

--- src/app/comentario/comentario.module.ts
+++ src/app/comentario/comentario.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ComentarioListComponent } from './comentario-list/comentario-list.component';
 import { ComentarioService } from './comentario.service';
 import { ComentarioDetailComponent } from './comentario-detail/comentario-detail.component';
-
+import { ComentarioRoutingModule } from './comentario-routing.module';
 @NgModule({
-  imports: [ CommonModule],
+  imports: [ CommonModule, ComentarioRoutingModule],
   declarations: [ComentarioListComponent, ComentarioDetailComponent],
   exports: [ComentarioListComponent],
   providers: [ComentarioService]
