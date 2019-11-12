b5771026ea06e23415ae94717f399096ae4b326e
Juan R <jd.ruedaa@uniandes.edu.co>
2019-11-11 20:48:44 -0500

    create comentario

--- src/app/comentario/comentario-create/comentario-create.component.ts
+++ src/app/comentario/comentario-create/comentario-create.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 
-//import {ToastrService} from 'ngx-toastr';
+import {ToastrService} from 'ngx-toastr';
 import {ComentarioService} from '../comentario.service';
 import {Comentario} from '../comentario';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'comentario-create',
@@ -12,15 +13,25 @@ import {Comentario} from '../comentario';
 export class ComentarioCreateComponent implements OnInit {
 
   constructor(
-   
     private comentarioService: ComentarioService,
-    //private toastrService: ToastrService
+    private toastrService: ToastrService
   ) { }
+    
+    formC = new FormGroup({
+      calificacion: new FormControl(1),
+      texto: new FormControl('texto'),
+    });
 
     /**
     * The new comentario
     */
     comentario: Comentario;
+
+    onSubmit(): void {
+      this.comentario.calificacion = this.formC.get('calificacion').value;  
+      this.comentario.texto = this.formC.get('texto').value;
+      this.createComentario();
+    }
 
     /**
     * The output which tells the parent component
@@ -37,18 +48,17 @@ export class ComentarioCreateComponent implements OnInit {
     /**
     * Creates a comentario
     */
-    createComentario(): Comentario {
-      
-        //this.comentarioService.createComentario(this.comentario)
-            //.subscribe((comentario) => {
-                //this.comentario = comentario;
-                //this.create.emit();
-                //this.toastrService.success("The comentario was created", "comentario creation");
+    createComentario(): Comentario { 
+        this.comentarioService.createComentario(this.comentario)
+            .subscribe((comentario : Comentario) => {
+                this.comentario = comentario;
+                this.create.emit();
+                this.toastrService.success("The comentario was created", "comentario creation");
 
-            //});
+            });
         return this.comentario;
     }
-
+    
     /**
     * Emits the signal to tell the parent component that the
     * user no longer wants to create a comentario
