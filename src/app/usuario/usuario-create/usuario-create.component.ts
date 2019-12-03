4ce6f1b399eed73da3d5fbdb25cbade48b6d5e6a
Juan Sebastián González Rojas <js.gonzalez15@uniandes.edu.co>
2019-12-03 17:23:27 -0500

    Agregados métodos en servicio que permiten creación de roles y autenticación, revisar duda de rol en usuario-create (ahora comentada) con profesor

--- src/app/usuario/usuario-create/usuario-create.component.ts
+++ src/app/usuario/usuario-create/usuario-create.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'; //librerías para relación entre formularios y componentes
+
 import { Usuario} from '../usuario';
-import { UsuarioService} from '../usuario.service';
+import { UsuarioService} from '../usuario.service'; //Comunicación con servicios 
 import { ToastrService} from 'ngx-toastr';
 
 @Component({
@@ -9,11 +10,18 @@ import { ToastrService} from 'ngx-toastr';
   templateUrl: './usuario-create.component.html',
   styleUrls: ['./usuario-create.component.css']
 })
-export class UsuarioCreateComponent {
+export class UsuarioCreateComponent implements OnInit{
 
   usuarioForm : FormGroup; //formulario de usuario con objetos de tipo FormControl
   usuarios: Usuario[];
-
+  usuario: Usuario;
+  roles: String[];// roles definidos para usuarios
+  /**
+    * Constructor for the component
+    * @param UsuarioService Servicios para actualización de métodos
+    * @param toastrService The toastr to show messages to the user
+    * @param formBuilder Form Builder para creación de formulario
+    */
   constructor(private servicio: UsuarioService, private formBuilder:FormBuilder,
    private toastr: ToastrService) {
     this.usuarios=[];
@@ -22,6 +30,7 @@ export class UsuarioCreateComponent {
     correo:["",[Validators.required,Validators.email]],
     password:["",[Validators.required,Validators.minLength(8)]]});
    }
+   /** Create: Crea el usuario utilizando el comando push y el servicio UsuarioService*/
   createUsuario(newUsuario: Usuario) {
     this.servicio.createUsuario(newUsuario).subscribe((usuario:Usuario) => {
       this.usuarios.push(usuario);
@@ -31,9 +40,19 @@ export class UsuarioCreateComponent {
     
     this.usuarioForm.reset();
   }
+   /** Loggear al usuario con el role seleccionado  */
+   signUp(): void {
+    //this.servicio.login(this.usuario.roles);
+    this.toastr.success('Successfully signed up')
+  }
 
+  /** Success: Mensaje de éxito para creación de usuario*/ 
   showSuccess() {
     this.toastr.success('El usuario se creó exitosamente');
-
   }
+  /** OnInit: Función inicial para creación de usuario   */
+   ngOnInit() {
+    this.usuario = new Usuario();
+    this.roles = ['ADMIN', 'USER','UNLOGGED'];
+}
 }
--- src/app/usuario/usuario.service.ts
+++ src/app/usuario/usuario.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, of } from "rxjs";
+import {NgxRolesService, NgxPermissionsService} from 'ngx-permissions'
+import {Router} from '@angular/router';
+
 import { Usuario} from "./usuario"
 
 const backUrl = "http://localhost:8080/s3_bicitours-api/api"; // URL to web api
@@ -9,8 +12,71 @@ const usuarios = "/usuario";
 @Injectable()
 export class UsuarioService {
 
-  constructor(private http: HttpClient) { }
+  /** Constructor del servicio Usuario
+     * @param router Router de angular para redireccionar al usuario en login o logout
+     * @param roleService NgxRolesService para manejar autenticaciones de rol (según ejemplo de front-pregrado201920)
+     * @param permissionsService NgxPermissionsService para manejar autenticaciones de rol
+     */
+  constructor(private router: Router, private roleService: NgxRolesService,
+     private permissionsService: NgxPermissionsService, private http: HttpClient) { }
   
+     //Funciones para creación y cambio de autenticación (según front-pregrado201920)
+  /** Start: reinicia los estados de autenticación*/
+  start (): void {
+    this.permissionsService.flushPermissions();
+    this.roleService.flushRoles();
+    this.permissionsService.loadPermissions(['edit_author_permission', 'delete_author_permission', 'leave_review']);
+    const role = localStorage.getItem('role');
+    if (!role) {
+        this.setUnloggedRole();
+    } else if (role === 'ADMIN') {
+        this.setAdministratorRole();
+    } else {
+        this.setUserRole();
+    }
+}
+/** No registrado: actualiza el usuario a UNLOGGED */
+setUnloggedRole (): void {
+    this.roleService.flushRoles();
+    this.roleService.addRole('UNLOGGED', ['']);
+}
+/** Registrado: actualiza el usuario a USER */
+setUserRole (): void {
+    this.roleService.flushRoles();
+    this.roleService.addRole('USER', ['leave_review']);
+    localStorage.setItem('role', 'USER');
+}
+/** Administrador: actualiza el usuario a ADMIN */
+setAdministratorRole (): void {
+    this.roleService.flushRoles();
+    this.roleService.addRole('ADMIN', ['edit_author_permission', 'delete_author_permission']);
+    localStorage.setItem('role', 'ADMIN');
+}
+/** Imprimir rol: imprime en consola el rol del usuario actual */
+printRole (): void {
+    console.log(this.roleService.getRoles());
+}
+
+/** Loggea el usuario en el rol deseado
+ * @param role El rol deseado a registrar */
+login (role): void {
+    if (role === 'ADMIN') {
+        this.setAdministratorRole();
+    } else {
+        this.setUserRole()
+    }
+    this.router.navigateByUrl('/home');
+}
+
+/** Desconectar: desconecta al usuario cambiando su rol a UNLOGGED */
+logout (): void {
+    this.roleService.flushRoles();
+    this.setUnloggedRole();
+    localStorage.removeItem('role');
+    this.router.navigateByUrl('/');
+}
+
+  // Funciones para comunicación con Back mediante Html
   /** GET usuarios del server */
   getUsuarios(): Observable<Usuario[]> {
     return this.http.get<Usuario[]>(backUrl+usuarios);
@@ -34,5 +100,5 @@ export class UsuarioService {
   updateUsuario(usuarioActualizado: Usuario): Observable<any> {
     return this.http.put(backUrl+usuarios+"/"+usuarioActualizado.id, usuarioActualizado);
   }
-
+  
 }
